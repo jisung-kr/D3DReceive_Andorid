3a2b8a3715202d4314e4be54a79f92bd9e633ce1
s-jis <s-jis@naver.com>
2019-09-24 13:54:36 +0900

    변경

--- D3DReceive_Andorid/D3DReceive_Andorid.NativeActivity/Receiver.h
+++ D3DReceive_Andorid/D3DReceive_Andorid.NativeActivity/Receiver.h
@@ -1,66 +1,63 @@
 #pragma once
-#pragma once
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
 
 #define BUFFER_SIZE 1024
-#define PORT 3500
+#define PORT 45000
 
 //#define SERVER_IP "127.0.0.1"
 //#define SERVER_IP "61.73.65.218"
-#define SERVER_IP "59.10.192.66"
+#define SERVER_IP "119.192.192.116"
 
 enum COMMAND {
-	//임시 명령
 	COMMAND_REQ_FRAME = 0,
-	COMMAND_RES_FRAME = 1
+	COMMAND_RES_FRAME = 1,
+	COMMAND_INPUT_KEY,
+	COMMAND_MAX
 };
 
+enum INPUT_TYPE {
+	INPUT_KEY_W = 0,
+	INPUT_KEY_S = 1,
+	INPUT_KEY_A = 2,
+	INPUT_KEY_D = 3,
+	INPUT_MAX
+};
+
+struct INPUT_DATA {
+	INPUT_TYPE mInputType;
+	float x;
+	float y;
+	float z;
+};
 
 struct HEADER {
-	unsigned int mDataLen;
-	unsigned short mCommand;
-	unsigned char mMsgNum;
-	unsigned char mMsgTotalNum;
+	unsigned long mDataLen;
+	unsigned long mCommand;
 };
 
+//헤더 생성 보조 구조체
 struct CHEADER : HEADER {
 	CHEADER() {
 		mDataLen = 0;
-		mCommand = COMMAND::COMMAND_REQ_FRAME;
-		mMsgNum = 0;
-		mMsgTotalNum = 0;
+		mCommand = htonl(COMMAND::COMMAND_REQ_FRAME);
 	}
 
-	CHEADER(unsigned short command) {
+	CHEADER(unsigned long command) {
 		mDataLen = 0;
-		mCommand = command;
-		mMsgNum = 0;
-		mMsgTotalNum = 0;
+		mCommand = htonl(command);
 	}
-	CHEADER(unsigned short command, unsigned int dataLen) {
-		mDataLen = dataLen;
-		mCommand = command;
-		mMsgNum = 0;
-		mMsgTotalNum = 0;
-	}
-
-	CHEADER(unsigned int dataLen, unsigned short command, unsigned char msgNum) {
-		mDataLen = dataLen;
-		mCommand = command;
-		mMsgNum = msgNum;
-		mMsgTotalNum = msgNum;
-	}
-
-	CHEADER(unsigned int dataLen, unsigned short command, unsigned char msgNum, unsigned char msgTotNum) {
-		mDataLen = dataLen;
-		mCommand = command;
-		mMsgNum = msgNum;
-		mMsgTotalNum = msgTotNum;
+	CHEADER(unsigned long command, unsigned long dataLen) {
+		mDataLen = htonl(dataLen);
+		mCommand = htonl(command);
 	}
 };
 
-struct NETWORK_MSG {
-	HEADER header;
-	char* data;
+struct WSABUF {
+	char* buf;
+	unsigned long len;
 };
 
 class Client {
@@ -69,20 +66,29 @@ public:
 	virtual ~Client();
 
 private:
-
 	int serverSock;
 	sockaddr_in serverAddr;
 
-	HEADER resHeader;
-	void* data = nullptr;
+	
+	WSABUF wsaReadBuf[2];
+	WSABUF wsaWriteBuf[2];
+
+	unsigned long headerSize = sizeof(HEADER);
 
 public:
 	bool Init();
 	bool Connection();
+
+	bool RecvMSG();
+	bool SendMSG(HEADER header, void* data = nullptr);
+
 	char* GetData();
-
-	bool Request(HEADER header, void* data = nullptr);
-	bool RecvResponse();
-
 	void ReleaseBuffer();
+
+private:
+	bool RecvHeader();
+	bool SendHeader();
+
+	bool RecvData();
+	bool SendData();
 };
